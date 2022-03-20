c79c7d8da1bae93d7916a654968cdf83ddf1d840
SungHo Ahn <jugiaro95@gmail.com>
2022-03-20 14:18:55 +0900

    :o: [Inflearn MSA] Spring Cloud Gateway - Load Balancer

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,10 +1,14 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 // 1-1. 우리가 만든 주소: http:localhost:8081/welcome
 // 1-2. apigateway로 전달받은 요청: http://localhost:8081/first-service/**
@@ -12,6 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service") // 1-3. path에 맞게 들어올 수 있게 RequestMapping 설정
 @Slf4j // 2-1. Lombok 로그 사용하기 위함
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -27,7 +38,9 @@ public class FirstServiceController {
 
     // [API Gateway Service] - Custom Filter 적용
     @GetMapping("/check")
-    public String check() {
-        return "Hi, Custom Filter from First Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Hi, Custom Filter from First Service on Port %s", env.getProperty("local.server.port"));
     }
 }
